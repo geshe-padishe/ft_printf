c870d5b51e6188d839600a97fe057d5b36b69e3b
Nikolov Niki <nikotikcho@Nikolovs-MacBook-Pro.local>
2020-12-11 21:46:43 +0100

    insert dynarray first prototype

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nikotikcho <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 00:16:14 by nikotikch         #+#    #+#             */
-/*   Updated: 2020/12/10 00:07:14 by nikotikch        ###   ########.fr       */
+/*   Updated: 2020/12/11 12:29:41 by nikotikch        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,41 @@
 //
 //	return (0);
 //}
-//int test_(void)
-//{
-//
-//	return (0);
-//}
-int test_push(void)
+int test_dynarray_extract(void)
+{
+	t_dynarray dynarray;
+	char *list;
+	char *tmp;
+
+	init_dynarray(&dynarray, (uint64_t)4, (size_t)1);
+	push_dynarray(&dynarray, "abcd", 4, true);
+
+	dynarray_extract(&dynarray, 2, 1);
+	list = dynarray.list;
+	tmp = dynarray.tmp;
+	assert(list[2] == 'd');
+	assert(dynarray.nb_cells == 3);
+
+	dynarray_extract(&dynarray, 2, 1);
+	list = dynarray.list;
+	tmp = dynarray.tmp;
+	assert(list[1] == 'b');
+	assert(dynarray.nb_cells == 2);
+
+	dynarray_extract(&dynarray, 1, 1);
+	list = dynarray.list;
+	tmp = dynarray.tmp;
+	assert(list[0] == 'a');
+	assert(dynarray.nb_cells == 1);
+
+
+	assert(dynarray_extract(NULL, 5, 3) == -1);
+	assert(dynarray_extract(&dynarray, 4, 7) == -1);
+	assert(dynarray_extract(&dynarray, 0, 10) == -1);
+	return (0);
+}
+
+int test_push_dynarray(void)
 {
 	t_dynarray dynarray;
 	char *list;
@@ -44,12 +73,12 @@ int test_push(void)
 	list = dynarray.list;
 	tmp = dynarray.tmp;
 
-	assert(push(NULL, "3\0", 2, false) == -1);
-	assert(push(&dynarray, NULL, 2, false) == -1);
-	assert(push(&dynarray, "3\0", 0, false) == -1);
+	assert(push_dynarray(NULL, "3\0", 2, false) == -1);
+	assert(push_dynarray(&dynarray, NULL, 2, false) == -1);
+	assert(push_dynarray(&dynarray, "3\0", 0, false) == -1);
 
 	printf("byte_size: %llu\n", dynarray.byte_size);
-	push(&dynarray, "abcd", 4, true);
+	push_dynarray(&dynarray, "abcd", 4, true);
 	printf("byte_size: %llu\n", dynarray.byte_size);
 
 	list = dynarray.list;
@@ -59,7 +88,7 @@ int test_push(void)
 	printf("nb_cells: %llu\n", dynarray.nb_cells);
 	printf("byte_size: %llu\n", dynarray.byte_size);
 	printf("list add: %p\n", dynarray.list);
-	push(&dynarray, "3\0", 2, false);
+	push_dynarray(&dynarray, "3\0", 2, false);
 	printf("list add: %p\n", dynarray.list);
 
 	list = dynarray.list;
@@ -68,7 +97,7 @@ int test_push(void)
 	printf("byte_size: %llu\n", dynarray.byte_size);
 	assert(list[5] == '\0');
 
-	push(&dynarray, "33", 2, true);
+	push_dynarray(&dynarray, "33", 2, true);
 
 	list = dynarray.list;
 	tmp = dynarray.tmp;
@@ -76,18 +105,19 @@ int test_push(void)
 
 	return (0);
 }
-int test_pop(void)
+
+int test_pop_dynarray(void)
 {
 	t_dynarray dynarray;
 
 	init_dynarray(&dynarray, 10, 1);
 	memcpy(dynarray.list, "kishta123", 10);
 	dynarray.nb_cells += 10;
-	pop(&dynarray, 1, true);
+	pop_dynarray(&dynarray, 1, true);
 	assert(((char*)dynarray.list)[0] == 'i');
-	pop(&dynarray, 2, true);
+	pop_dynarray(&dynarray, 2, true);
 	assert(((char*)dynarray.list)[0] == 'h');
-	pop(&dynarray, 10, true);
+	pop_dynarray(&dynarray, 10, true);
 	assert(((char*)dynarray.list)[0] == 'h');
 	return (0);
 }
@@ -128,8 +158,9 @@ int main(int argc, char **argv)
 	init_dynarray(&dynarray, (uint64_t)atoi(argv[1]), (size_t)atoi(argv[2]));
 	test_init_dynarray();
 	test_free_dynarray();
-	test_pop();
-	test_push();
+	test_pop_dynarray();
+	test_push_dynarray();
+	test_dynarray_extract();
 	ft_memcpy(dynarray.list, "abcd", 4);
 	dynarray.nb_cells += 4;
 ////	printf("nb_cells of dynarray: %llu\n", dynarray.nb_cells);
@@ -150,7 +181,7 @@ int main(int argc, char **argv)
 ////	printf("nb_cells of dynarray: %llu\n", dynarray.nb_cells);
 ////	printf("list: %s\n\n", dynarray.list);
 ////
-	ret = push(&dynarray, "3\0", 2, false);
+	ret = push_dynarray(&dynarray, "3\0", 2, false);
 
 	printf("nb_cells of dynarray: %llu\n", dynarray.nb_cells);
 	printf("cells_size of dynarray: %zu\n", dynarray.cell_size);
