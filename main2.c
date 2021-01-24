7b37c47b7a1c053d8b2ba861c40e88f5e42b079c
Nikolov Niki <nikotikcho@Nikolovs-MacBook-Pro.local>
2021-01-24 14:05:13 +0100

    hex converter

--- main2.c
+++ main2.c
@@ -3,7 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "libft.h"
+//#include "libft.h"
+
+void write_hex(unsigned long long nb)
+{
+	if (nb > 15)
+		write_hex(nb / 16);
+	write(1, &"0123456789abcdef"[nb % 16], 1);
+}
 
 int main()
 {
@@ -49,66 +56,60 @@ int main()
 //	printf("gloat .20: %.20g\n", number);
 //	printf("float .20: %.20f\n", number);
 //
-	printf("----------------------'---------------------\n");
-
-	printf("'.10e: flag ''' bad behavior with 'e'\n");
-	printf("'.10d: %'.10d\n", number2);
-	printf("'.10g: %'.10g\n", number);
-	printf("'.10f: %'.10f\n", number);
-	setlocale(7, "LC_NUMERIC");
-	printf("localeconv(3): \"%c\"\n", *(localeconv()->thousands_sep));
-
-	printf("---------------------o-octal-----------------\n");
-
-	printf("+o: + undefined with o\n");
-	printf(" o: ' ' undefined with o\n");
-	printf("0o: 0 undefined with o\n");
-	printf("-o: %-o\n", integer);
-	printf("#o: %#o\n", integer);
-	printf("#-o: %-#o\n", integer);
-	printf("-15.10o: %-15.10o\n", integer);
-	printf("15.10o: %15.10o\n", integer);
-	printf("#.10o: %#.10o\n", integer);
-	printf("0.10o: %0.10o\n", integer);
-
-	printf("-----------first-flag-combinations---------------\n");
-
-	printf("-0: - overrides 0");
-	printf("0#x: %#x\n", integer);
-	printf("-#x: %-#x\n", integer);
-	printf(" 0d: % 0d\n", integer);
-	printf("+0d: % 0d\n", integer);
-
+//	printf("----------------------'---------------------\n");
+//
+//	printf("'.10e: flag ''' bad behavior with 'e'\n");
+//	printf("'.10d: %'.10d\n", number2);
+//	printf("'.10g: %'.10g\n", number);
+//	printf("'.10f: %'.10f\n", number);
+//	setlocale(7, "LC_NUMERIC");
+//	printf("localeconv(3): \"%c\"\n", *(localeconv()->thousands_sep));
+//
+//	printf("---------------------o-octal-----------------\n");
+//
+//	printf("+o: + undefined with o\n");
+//	printf(" o: ' ' undefined with o\n");
+//	printf("0o: 0 undefined with o\n");
+//	printf("-o: %-o\n", integer);
+//	printf("#o: %#o\n", integer);
+//	printf("#-o: %-#o\n", integer);
+//	printf("-15.10o: %-15.10o\n", integer);
+//	printf("15.10o: %15.10o\n", integer);
+//	printf("#.10o: %#.10o\n", integer);
+//	printf("0.10o: %0.10o\n", integer);
+//
+//	printf("-----------first-flag-combinations---------------\n");
+//
+//	printf("-0: - overrides 0");
+//	printf("0#x: %#x\n", integer);
+//	printf("-#x: %-#x\n", integer);
+//	printf(" 0d: % 0d\n", integer);
+//	printf("+0d: % 0d\n", integer);
+//
 	printf("-------------------My-stuff-----------------------\n");
-	write(1, ptr, 8);
-	write(1, "hello\n", 6);
-	printf("ptr: %lx\n", ptr);
-	printf("ptr: %p\n", ptr);
-
 	long address;
 	long long address2;
-	char *str;
 
 	address = (long)ptr;
 	address2 = (long long)ptr;
-	printf("address: %lx\n", address);	
-	printf("sizeof(address): %lu\n", sizeof(address));
-	printf("sizeof(address2): %lu\n", sizeof(address2));
-	printf("sizeof(long): %lu\n", sizeof(long));
-	printf("sizeof(long long): %lu\n", sizeof(long long));
-	printf("sizeof(int): %lu\n", sizeof(int));
-	
-	str = ft_itoa(address);
-	printf("address: %lu\n", address % 16);
-	address = address / 16;
-	printf("address: %lu\n", address % 16);
-	address = address / 16;
-	printf("address: %lu\n", address % 16);
-	address = address / 16;
-	printf("address: %lu\n", address % 16);
-	address = address / 16;
-	printf("address: %lu\n", address % 16);
-	address = address / 16;
-	printf("address: %lu\n", address % 16);
-	printf("str: %s\n", str);
+	address2 = -0;
+	printf("address2 hex: %#.10llx\n", address2);	
+
+	dprintf(1, "address2 hex: 0x");
+	write_hex(address2);
+	printf("\n");
+	//str = ft_itoa(address);
+	//printf("address: %lu\n", address % 16);
+	//address = address / 16;
+	//printf("address: %lu\n", address % 16);
+	//address = address / 16;
+	//printf("address: %lu\n", address % 16);
+	//address = address / 16;
+	//printf("address: %lu\n", address % 16);
+	//address = address / 16;
+	//printf("address: %lu\n", address % 16);
+	//address = address / 16;
+	//printf("address: %lu\n", address % 16);
+	//printf("str: %s\n", str);
+
 }
